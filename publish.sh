db2f3f9d4d64a5f2abe7ddf43b18fc209850c943
evan <evanljp@outlook.com>
2018-11-03 19:06:12 +0800

    提交新版本

--- publish.sh
+++ publish.sh
@@ -3,17 +3,19 @@ echo "build this gitbook...."
 gitbook build
 echo "create and checkout new branch gh-pages...."
 echo "清理文件...."
-echo "_book" > .gitignore
+git branch -D gh-pages
+git checkout -b gh-pages
 git rm --cached -r .
 git clean -df
 echo "create .gitignore...."
 echo "*~" > .gitignore
 echo "_book" >> .gitignore
+echo "node_modules" >> .gitignore
 echo "commit and push...."
 cp -r _book/* .
 git add -A
 git commit -am "publish this book"
-git push origin -u  gh-pages
+git push origin -u  gh-pages --force
 echo "finished!!!"
 git checkout master
 
