f045f4947140905312ebe5eb79968825bd589f5f
Evan <evanljp@outlook.com>
2019-01-27 15:08:35 +0800

    code

--- book/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-echo "gitbook build...."
-gitbook build
-echo "gitbook serve"
-gitbook serve
-echo "finished!!!"
--- book/push.sh
+++
@@ -1,7 +0,0 @@
-echo "编译gitbook"
-gitbook build
-echo "上传中....."
-git add -A
-git commit -am "提交新版本"
-git push -u origin master
-echo "|||||||||||||||FINISHED|||||||||||||||||||"
